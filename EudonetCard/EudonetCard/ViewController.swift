8d90ed04a06350081c1e61f03935eb701163d817
MacbookPRV <roger@MacbookPRV.local>
2016-07-13 16:26:10 +0200

    iphone projet

---
+++ EudonetCard/EudonetCard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EudonetCard
+//
+//  Created by MacbookPRV on 13/07/2016.
+//  Copyright © 2016 Pastouret Roger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
